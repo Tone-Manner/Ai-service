e0249d93a3adcc82673f753f072aeceebbf3d649
choido00 <gisang0927@gmail.com>
2026-07-30 22:07:56 +0900

    feat(exception, swagger): 글로벌 예외 처리기 장착 및 스웨거 연동

--- src/main/java/com/textrefiner/aiservice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/textrefiner/aiservice/exception/GlobalExceptionHandler.java
@@ -10,6 +10,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    // 1. 외부 API(Gemini) 통신 관련 에러 (Timeout, 429 과도한 요청 등)
+    @ExceptionHandler(FeignException.class)
+    public ResponseEntity<ErrorResponse> handleFeignException(FeignException e) {
+        // 프론트엔드가 메시지로 덮어씌움
+        ErrorResponse response = new ErrorResponse(
+                HttpStatus.SERVICE_UNAVAILABLE.value(), // 503 에러
+                "현재 AI 서비스가 혼잡하거나 지연되고 있습니다. 잠시 후 다시 시도해주세요."
+        );
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(response);
+    }
 
     // 2. 그 외 예상치 못한 모든 에러
     @ExceptionHandler(Exception.class)
